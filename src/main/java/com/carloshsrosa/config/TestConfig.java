0608edcf76ef76478ffcebd1c7a845f8a7aee9b5
Carlos Viegas <carloshsrosa@gmail.com>
2025-02-17 10:55:44 -0300

    Operação de instanciação da base de dados

---
+++ src/main/java/com/carloshsrosa/config/TestConfig.java
@@ -0,0 +1,32 @@
+package com.carloshsrosa.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.carloshsrosa.entities.User;
+import com.carloshsrosa.repositories.UserRepository;
+
+@Configuration
+@Profile("test")
+public class TestConfig implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		userRepository.deleteAll();
+
+		User u1 = new User(null, "Maria Brown", "maria@gmail.com");
+		User u2 = new User(null, "Alex Green", "alex@gmail.com");
+		User u3 = new User(null, "Bob Grey", "bob@gmail.com");
+
+		userRepository.saveAll(Arrays.asList(u1, u2, u3));
+
+	}
+
+}
