f2937016c29d75afa315088fa5acc2345ca33881
Carlos Viegas <carloshsrosa@gmail.com>
2025-02-17 12:22:24 -0300

    Inserção de usuário com POST

--- src/main/java/com/carloshsrosa/services/UserService.java
+++ src/main/java/com/carloshsrosa/services/UserService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.carloshsrosa.dto.UserDTO;
 import com.carloshsrosa.entities.User;
 import com.carloshsrosa.exceptions.ObjectNotFoundException;
 import com.carloshsrosa.repositories.UserRepository;
@@ -25,5 +26,13 @@ public class UserService {
 			throw new ObjectNotFoundException("Object not found");
 		}
 		return user;
+	}
+
+	public User insert(User user) {
+		return repository.insert(user);
+	}
+
+	public User fromDTO(UserDTO userDTO) {
+		return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
 	}
 }
