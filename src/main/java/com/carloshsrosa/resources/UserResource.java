a324dbbe77a6e6cf5426acb2b37fec5783640acb
Carlos Viegas <carloshsrosa@gmail.com>
2025-02-17 12:01:48 -0300

    Obtendo um usuário por id

--- src/main/java/com/carloshsrosa/resources/UserResource.java
+++ src/main/java/com/carloshsrosa/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,10 +21,15 @@ public class UserResource {
 	private UserService service;
 
 	@GetMapping
-	public ResponseEntity<List<UserDTO>> users() {
+	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> users = service.findAll();
 		List<UserDTO> usersDTO = users.stream().map(u -> new UserDTO(u)).toList();
 		return ResponseEntity.ok().body(usersDTO);
 	}
 
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		User user = service.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(user));
+	}
 }
--- src/main/java/com/carloshsrosa/services/UserService.java
+++ src/main/java/com/carloshsrosa/services/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.carloshsrosa.entities.User;
+import com.carloshsrosa.exceptions.ObjectNotFoundException;
 import com.carloshsrosa.repositories.UserRepository;
 
 @Service
@@ -16,5 +17,13 @@ public class UserService {
 
 	public List<User> findAll() {
 		return repository.findAll();
+	}
+
+	public User findById(String id) {
+		User user = repository.findById(id).orElse(null);
+		if (user == null) {
+			throw new ObjectNotFoundException("Object not found");
+		}
+		return user;
 	}
 }
