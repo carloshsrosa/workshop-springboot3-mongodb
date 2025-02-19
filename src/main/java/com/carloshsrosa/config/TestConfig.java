a08252e23a2105552f815ed3933e0d51409ac6f0
Carlos Viegas <carloshsrosa@gmail.com>
2025-02-19 15:03:04 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/carloshsrosa/config/TestConfig.java
+++ src/main/java/com/carloshsrosa/config/TestConfig.java
@@ -8,6 +8,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.carloshsrosa.dto.AuthorDTO;
 import com.carloshsrosa.entities.Post;
 import com.carloshsrosa.entities.User;
 import com.carloshsrosa.repositories.PostRepository;
@@ -32,11 +33,13 @@ public class TestConfig implements CommandLineRunner {
 		User u2 = new User(null, "Alex Green", "alex@gmail.com");
 		User u3 = new User(null, "Bob Grey", "bob@gmail.com");
 
-		Post p1 = new Post(null, Instant.parse("2018-03-21T14:29:53.123498Z"), "Partiu viagem",
-				"Vou viajar para São Paulo. Abraços!", u1);
-		Post p2 = new Post(null, Instant.parse("2018-03-23T04:57:13.561576Z"), "Bom dia", "Acordei feliz hoje!", u1);
-
 		userRepository.saveAll(Arrays.asList(u1, u2, u3));
+
+		Post p1 = new Post(null, Instant.parse("2018-03-21T14:29:53.123498Z"), "Partiu viagem",
+				"Vou viajar para São Paulo. Abraços!", new AuthorDTO(u3));
+		Post p2 = new Post(null, Instant.parse("2018-03-23T04:57:13.561576Z"), "Bom dia", "Acordei feliz hoje!",
+				new AuthorDTO(u3));
+
 		postRepository.saveAll(Arrays.asList(p1, p2));
 
 	}
