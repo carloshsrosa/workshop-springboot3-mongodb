6089eee6e9548e8e99e3d12302fbf0400824f682
Carlos Viegas <carloshsrosa@gmail.com>
2025-02-19 15:59:52 -0300

    Consulta simples com query methods

--- src/main/java/com/carloshsrosa/resources/PostResource.java
+++ src/main/java/com/carloshsrosa/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.carloshsrosa.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.carloshsrosa.entities.Post;
+import com.carloshsrosa.resources.util.URL;
 import com.carloshsrosa.services.PostService;
 
 @RestController
@@ -23,4 +27,10 @@ public class PostResource {
 		return ResponseEntity.ok().body(post);
 	}
 
+	@GetMapping(value = "/titlesearch")
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+		text = URL.decodeParam(text);
+		List<Post> posts = service.findByTitle(text);
+		return ResponseEntity.ok().body(posts);
+	}
 }
