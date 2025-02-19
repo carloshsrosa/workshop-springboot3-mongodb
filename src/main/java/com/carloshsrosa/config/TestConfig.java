a5dd4839d4167491191ceb2552aee8af849a305b
Carlos Viegas <carloshsrosa@gmail.com>
2025-02-19 14:57:32 -0300

    Criando entity Post com User aninhado

--- src/main/java/com/carloshsrosa/config/TestConfig.java
+++ src/main/java/com/carloshsrosa/config/TestConfig.java
@@ -1,5 +1,6 @@
 package com.carloshsrosa.config;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +8,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.carloshsrosa.entities.Post;
 import com.carloshsrosa.entities.User;
+import com.carloshsrosa.repositories.PostRepository;
 import com.carloshsrosa.repositories.UserRepository;
 
 @Configuration
@@ -17,15 +20,24 @@ public class TestConfig implements CommandLineRunner {
 	@Autowired
 	private UserRepository userRepository;
 
+	@Autowired
+	private PostRepository postRepository;
+
 	@Override
 	public void run(String... args) throws Exception {
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com");
 		User u3 = new User(null, "Bob Grey", "bob@gmail.com");
 
+		Post p1 = new Post(null, Instant.parse("2018-03-21T14:29:53.123498Z"), "Partiu viagem",
+				"Vou viajar para São Paulo. Abraços!", u1);
+		Post p2 = new Post(null, Instant.parse("2018-03-23T04:57:13.561576Z"), "Bom dia", "Acordei feliz hoje!", u1);
+
 		userRepository.saveAll(Arrays.asList(u1, u2, u3));
+		postRepository.saveAll(Arrays.asList(p1, p2));
 
 	}
 
